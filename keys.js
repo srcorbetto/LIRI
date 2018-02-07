f35a8611eeb42005e5d24c7d1abb26902c0feb00
Mike Corbett <srcorbetto@gmail.com>
2018-02-06 23:58:25 -0600

    Proper connections established

--- keys.js
+++ keys.js
@@ -11,3 +11,7 @@ exports.spotify = {
   id: process.env.SPOTIFY_ID,
   secret: process.env.SPOTIFY_SECRET
 };
+
+exports.OMDB = {
+	key: process.env.OMDB_KEY
+};
--- liri.js
+++ liri.js
@@ -7,12 +7,17 @@ var Twitter = require('twitter');
 // Spotify require
 var Spotify = require('node-spotify-api');
 
+// request require
+var request = require("request");
+
 // Imports the keys file
 const keys = require("./keys.js");
 
 // Put keys through APIs
 var spotify = new Spotify(keys.spotify);
 var client = new Twitter(keys.twitter);
+
+var omdbKey = keys.OMDB.key;
 
 
 // Test log
@@ -22,6 +27,7 @@ var client = new Twitter(keys.twitter);
 
 // console.log("Hello");
 
+// Twitter
 // client.post('statuses/update', {status: 'Booyah!'}, function(error, tweet, response) {
 //   if (!error) {
 //     console.log(tweet.text);
@@ -29,11 +35,31 @@ var client = new Twitter(keys.twitter);
 //   }
 // });
 
-spotify.search({type: 'track', query: 'All the Small Things' }, function(err, data) {
-  if (err) {
-    return console.log('Error occurred: ' + err);
+client.get('statuses/user_timeline', {count: 20}, function(error, tweet, response) {
+  if (!error) {
+  	// console.log(tweet[0].text);
+  	for (i = 0; i < tweet.length; i++) {
+  		console.log(tweet[i].text);
+    	console.log(tweet[i].created_at);
+  	}
+    // console.log(response);
+  } else {
+  	console.log(error);
   }
- 
-console.log(data.tracks.items[0].album.artists[0].name); 
 });
+
+
+// spotify.search({type: 'track', query: 'All the Small Things' }, function(err, data) {
+//   if (err) {
+//     return console.log('Error occurred: ' + err);
+//   }
+ 
+// console.log(data.tracks.items[0].album.artists[0].name); 
+// });
+
+// request('http://www.omdbapi.com/?t=big&apikey=' + omdbKey, function (error, response, body) {
+//   // console.log('error:', error); // Print the error if one occurred
+//   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+//   console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
+// });
 
