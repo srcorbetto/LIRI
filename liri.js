4d9a3a7a0aad5beb4c59e30095ac08ee9c147f79
Mike Corbett <srcorbetto@gmail.com>
2018-02-08 00:52:59 -0600

    Logic is working. Error handling could use some fixing

--- liri.js
+++ liri.js
@@ -1,6 +1,12 @@
 // Allows the application to read the .env file
 require("dotenv").config();
 
+// Takes in user arguments
+var input = process.argv;
+
+// File structure
+var fs = require("fs");
+
 // Twitter require
 var Twitter = require('twitter');
 
@@ -19,13 +25,125 @@ var client = new Twitter(keys.twitter);
 
 var omdbKey = keys.OMDB.key;
 
+// Program
+function liri(command, optionalParam) {
 
-// Test log
-// console.log(keys.twitter.consumer_key);
-// console.log(spotify);
-// console.log(client);
+	if(command === "my-tweets") {
 
-// console.log("Hello");
+		//Twitter Search
+		client.get('statuses/user_timeline', {count: 20}, function(error, tweet, response) {
+		  if (!error) {
+		  	// Header
+		  	console.log("");
+		  	console.log("-----------------------------------------\n", " ", "Most recent tweets:", "\n-----------------------------------------");
+		  	for (i = 0; i < tweet.length; i++) {
+		  		console.log("");
+		  		console.log(tweet[i].text);
+		  		console.log("Date tweeted: " + tweet[i].created_at);
+		  		console.log("");
+		  	}
+		    // console.log(response);
+		  } else {
+		  	console.log(error);
+		  }
+		});
+
+	} else if (command === "spotify-this-song") {
+
+		// Spotify Search
+		spotify.search({type: 'track', query: optionalParam, limit: 1 }, function(err, data) {
+		  
+		  if (err) { //default Song
+		    
+			// Spotify Error New Search
+			spotify.search({type: 'track', query: "The Sign", limit: 1 }, function(err, data) {
+			  
+			  if (err) { //default Song
+			    
+				return console.log('Error occurred: ' + err);
+
+			  } else {
+
+			  	// Header
+			  	console.log("");
+			  	console.log("-----------------------------------------\n", "", "Error finding that song. Defaulting to Or Nah by Ty Dolla $ign:" + "\n-----------------------------------------");
+
+				console.log("Artist: " + data.tracks.items[0].artists[0].name);
+				console.log("Track: " + data.tracks.items[0].name);
+				console.log("Album: " + data.tracks.items[0].album.name);
+				console.log("Check this song out on Spotify: " + data.tracks.items[0].external_urls.spotify);
+				console.log("");
+
+			  }
+			 
+				   
+			});
+
+		  } else {
+
+		  	// Header
+		  	console.log("");
+		  	console.log("-----------------------------------------\n", "", optionalParam, "search:" + "\n-----------------------------------------");
+
+			console.log("Artist: " + data.tracks.items[0].artists[0].name);
+			console.log("Track: " + data.tracks.items[0].name);
+			console.log("Album: " + data.tracks.items[0].album.name);
+			console.log("Check this song out on Spotify: " + data.tracks.items[0].external_urls.spotify);
+			console.log("");
+
+		  }
+		 
+			   
+		});
+
+	} else if (command === "movie-this") {
+
+		// IMDB Search
+		request('http://www.omdbapi.com/?t=' + optionalParam + '&apikey=' + omdbKey, function (error, response, body) {
+		  if (error) {
+
+		  	console.log(error);
+
+		  } else {
+
+		  	// Header
+			  console.log("");
+			  console.log("-----------------------------------------\n", "", optionalParam, "movie search:" + "\n-----------------------------------------");
+
+			  console.log("Title: " + JSON.parse(body).Title);
+			  console.log("Year: " + JSON.parse(body).Year);
+			  console.log("IMDB Rating: " + JSON.parse(body).imdbRating);
+			  console.log("Rotten Tomatoes Score: " + JSON.parse(body).Ratings[1].Value);
+			  console.log("Country: " + JSON.parse(body).Country);
+			  console.log("Language: " + JSON.parse(body).Language);
+			  console.log("");
+			  console.log("Plot: " + JSON.parse(body).Plot);
+			  console.log("");
+			  console.log("Actors: " + JSON.parse(body).Actors);
+			  console.log("");
+
+		  }
+		  
+
+		});
+
+	} else if (command === "do-what-it-says") {
+
+		fs.readFile("random.txt", "utf8", function(err, data) {
+
+			if(err) {
+				return console.log(err); //return will stop there because it filled its purpose
+			}
+
+			liri("spotify-this-song", data);
+
+		});
+
+	}
+
+};
+
+liri(input[2], input[3]);
 
 // Twitter
 // client.post('statuses/update', {status: 'Booyah!'}, function(error, tweet, response) {
@@ -33,33 +151,5 @@ var omdbKey = keys.OMDB.key;
 //     console.log(tweet.text);
 //     // console.log(response);
 //   }
-// });
-
-client.get('statuses/user_timeline', {count: 20}, function(error, tweet, response) {
-  if (!error) {
-  	// console.log(tweet[0].text);
-  	for (i = 0; i < tweet.length; i++) {
-  		console.log(tweet[i].text);
-    	console.log(tweet[i].created_at);
-  	}
-    // console.log(response);
-  } else {
-  	console.log(error);
-  }
-});
-
-
-// spotify.search({type: 'track', query: 'All the Small Things' }, function(err, data) {
-//   if (err) {
-//     return console.log('Error occurred: ' + err);
-//   }
- 
-// console.log(data.tracks.items[0].album.artists[0].name); 
-// });
-
-// request('http://www.omdbapi.com/?t=big&apikey=' + omdbKey, function (error, response, body) {
-//   // console.log('error:', error); // Print the error if one occurred
-//   // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-//   console.log('body:', JSON.parse(body)); // Print the HTML for the Google homepage.
 // });
 
